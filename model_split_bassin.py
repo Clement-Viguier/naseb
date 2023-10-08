7f2a33498ee96aec1e5d4b233a114211586ffb27
Clement-Viguier <clem.viguier@wanadoo.fr>
2023-10-08 15:25:11 +0200

    clean model scripts

--- model_split_bassin.py
+++ model_split_bassin.py
@@ -2,24 +2,6 @@ from naseb.model import model_surf_park, model_pet_visual_crossing
 import numpy as np
 import pandas as pd
 
-MONTHLY_INDEX = [
-    "1992-01-01",
-    "1992-02-01",
-    "1992-03-01",
-    "1992-04-01",
-    "1992-05-01",
-    "1992-06-01",
-    "1992-07-01",
-    "1992-08-01",
-    "1992-09-01",
-    "1992-10-01",
-    "1992-11-01",
-    "1992-12-01",
-]
-
-MONTHLY_INDEX_30_YEARS = pd.read_csv('./data/monthly_data.csv')['date']
-
-
 VOLUME_BASSIN1 = 13000
 VOLUME_BASSIN2 = 7000
 
@@ -27,6 +9,7 @@ BASSIN_TOTAL_AREA = 13548
 
 tol = 0.01
 
+# technically only necessary once
 data = pd.read_csv(
     './data/canéjan interpolé 1992-01-01 to 2022-12-31.csv')
 model_pet_visual_crossing(data)
@@ -35,20 +18,8 @@ model_list = [
     "surf_park_no_city_water_split",
 ]
 
-index_dict = {
-    "surf_park_mensuel_copy": MONTHLY_INDEX,
-    "surf_park_monthly": MONTHLY_INDEX_30_YEARS
-}
-
 variations = {
     "baseline": {},
-    # "medium_evap": {"parameters": {
-    #     "pet_penalty_factor": {
-    #         "name": "pet_penalty_factor",
-    #         "type": "constant",
-    #         "value": 1.3,
-    #     }}
-    # },
     "high_evap": {"parameters": {
         "pet_penalty_factor": {
             "name": "pet_penalty_factor",
@@ -59,8 +30,6 @@ variations = {
     # "flow_limit":{} # add flow limits to the system's bottlenecks
 }
 
-# TODO need to check the link tampon -> rainwater_storage (other way around in schematics, but their sim assume such a link)
-
 agg_fct = {
     "bassin1_volume": [np.mean, np.median],
     "bassin2_volume": [np.mean, np.median],
@@ -70,12 +39,15 @@ agg_fct = {
 }
 
 sim_list = []
+# run the simulation for each model and parameter variant
 for model_name in model_list:
     for variation, extra_params in variations.items():
         print(f"Model: {model_name} - {variation}")
         model_path = f"./models/{model_name}.json"
         sim_df = model_surf_park(
-            model_path, index=index_dict.get(model_name, None), extra_params=extra_params)
+            model_path, index=None, extra_params=extra_params)
+
+        # compute additional features
         sim_df['bassin_volume'] = sim_df['bassin1_volume'] + \
             sim_df['bassin2_volume']
         sim_df['pet_flow'] = sim_df['pet_b1_flow'] + \
@@ -93,12 +65,14 @@ for model_name in model_list:
         sim_df['variation'] = variation
         sim_df['model_name'] = model_name
 
+        # save the simulation data
         sim_df.to_csv(f'./sim/sim_{model_name}_{variation}.csv')
         sim_list.append(sim_df)
 
 all_sims = pd.concat(sim_list)
 print(all_sims['capacity_group'])
 
+# summary statistics
 sim_summary = all_sims.groupby(
     ['variation', 'model_name']).aggregate(agg_fct)
 sim_summary.columns = sim_summary.columns.map('|'.join).str.strip('|')
